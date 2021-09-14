d65e86215a3f4abb2e600d4798967f556bc83390
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-09-14 13:40:20 +0700

    belajar Assets MVC

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
+<div class="container">
 
-    <h1>About Me</h1>
+    <h1 class="mt-4">About Me</h1> 
+    <img src="<?= BASEURL;?>/img/ProfilePhoto.jpg" alt="Muhamad Prasetyo" width="200" class="rounded-circle shadow">
     <p>Hallo, nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, saya adalah seorang <?= $data['pekerjaan']; ?> </p>
+</div>
