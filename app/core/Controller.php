c6495928b0c2a5406db1c4d8bd4ab66e08bd2223
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-09-14 13:13:54 +0700

    membuat alur view

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,8 +1,11 @@
 <?php 
 
 class Controller {
+
+    //  view 
     public function view($view, $data = []) 
     {
+        // untuk memanggil view pada folder views 
         require_once '../app/views/' . $view . '.php';
     }
 }
